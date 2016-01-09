5f8bc7eefa49dbb1474381b85995cf84488dcbec
cjstehno <chris@stehno.com>
2016-01-09 11:38:23 -0600

    Copied the sql migration into production resources... need to figure out how to share them!

--- src/main/resources/db/migration/V1_2__PhotoTables.sql
+++ src/main/resources/db/migration/V1_2__PhotoTables.sql
@@ -14,3 +14,61 @@
 -- limitations under the License.
 --
 
+CREATE TABLE photos (
+  id            SERIAL      NOT NULL PRIMARY KEY,
+  version       BIGINT    DEFAULT 0,
+  name          VARCHAR(50) NOT NULL,
+  description   VARCHAR(2000),
+  hash          VARCHAR(64),
+
+  date_uploaded TIMESTAMP DEFAULT now(),
+  date_updated  TIMESTAMP DEFAULT now(),
+  date_taken    TIMESTAMP,
+
+  geo_latitude  DOUBLE PRECISION,
+  geo_longitude DOUBLE PRECISION,
+  geo_altitude  INT
+);
+
+CREATE TABLE tags (
+  id       SERIAL      NOT NULL PRIMARY KEY,
+  category VARCHAR(20) NOT NULL,
+  label    VARCHAR(40) NOT NULL,
+  UNIQUE (category, label)
+);
+
+CREATE TABLE photo_tags (
+  photo_id BIGINT NOT NULL REFERENCES photos (id),
+  tag_id   BIGINT NOT NULL REFERENCES tags (id),
+  PRIMARY KEY (photo_id, tag_id)
+);
+
+CREATE TABLE images (
+  id             SERIAL      NOT NULL PRIMARY KEY,
+  scale          VARCHAR(10) NOT NULL,
+  width          INT         NOT NULL,
+  height         INT         NOT NULL,
+  content_length BIGINT      NOT NULL,
+  content_type   VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE photo_images (
+  photo_id BIGINT NOT NULL REFERENCES photos (id),
+  image_id BIGINT NOT NULL REFERENCES images (id),
+  PRIMARY KEY (photo_id, image_id)
+);
+
+CREATE TABLE albums (
+  id           SERIAL      NOT NULL PRIMARY KEY,
+  version      BIGINT    DEFAULT 0,
+  name         VARCHAR(50) NOT NULL,
+  description  VARCHAR(2000),
+  date_created TIMESTAMP DEFAULT now(),
+  date_updated TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE album_photos (
+  album_id BIGINT NOT NULL REFERENCES albums (id),
+  photo_id BIGINT NOT NULL REFERENCES photos (id),
+  PRIMARY KEY (album_id, photo_id)
+);
